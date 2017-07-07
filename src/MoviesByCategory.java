8f6b5b8d64b95a3a421ea5501b2a442a468fc680
Matthew Menna <mattmenna@gmail.com>
2017-07-07 16:44:21 -0400

    Initial Commit

--- src/MoviesByCategory.java
+++ src/MoviesByCategory.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Matt on 7/7/2017.
  */
 public class MoviesByCategory {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String category;
+
+        int numMovies = 0;
+        ArrayList<Movie> movieArray = new ArrayList<Movie>();
+        //Movie mvn = new Movie();
+
+        System.out.println("Welcome to the Movie List Application");
+
+        for (int i = 1; i < 101; i++) {
+            movieArray.add(Movie.getMovie(i));
+        }
+
+        System.out.println("There are "+ numMovies + " moviess in this list.");
+        System.out.println("What category are you interested in?:");
+        System.out.println("Options are animated, drama, horror, scifi");
+
+        System.out.println();
+
+        category = scnr.nextLine();
+
+
+
+        //System.out.println(movieArray.toString());
+
+    }
 }
